796f4a13c881aa1a769157afee11fa596a948aa0
Frank Bryden <frank.bryden@gmail.com>
2019-12-16 19:06:34 +0000

    Seems like it is working!

--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
-	"image/jpeg"
+	"image/png"
 	"os"
 	"strconv"
 )
 
+func padBitString(b string, n int) string {
+	for len(b) < n {
+		b = "0" + b
+	}
+	return b
+}
+
 func getInts(msg string) []int {
 	ints := make([]int, len(msg))
-	for i, c := range "abc" {
+	for i, c := range msg {
 		ints[i] = int(c)
 	}
 	return ints
@@ -21,9 +28,29 @@ func getInts(msg string) []int {
 func getBytes(ints []int) string {
 	strReprBinaryVals := ""
 	for _, val := range ints {
-		strReprBinaryVals += strconv.FormatInt(int64(val), 2)
+		s := strconv.FormatInt(int64(val), 2)
+		s = padBitString(s, 7)
+		strReprBinaryVals += s
 	}
 	return strReprBinaryVals
+}
+
+//read color.Color and return bit string of length 3 (RGB)
+func readPixel(col color.Color) string {
+	r, g, b, _ := col.RGBA()
+	r = r >> 8
+	g = g >> 8
+	b = b >> 8
+	cols := []uint32{r, g, b}
+	bitString := ""
+	for _, c := range cols {
+		if c%2 == 0 {
+			bitString += "0"
+		} else {
+			bitString += "1"
+		}
+	}
+	return bitString
 }
 
 //Flip parity
@@ -42,7 +69,7 @@ func convertImage(img image.Image) *image.RGBA {
 	return i
 }
 
-func decodeImage(img *image.RGBA) {
+func decodeMessage(img *image.RGBA) {
 	//WHAT REALLY NEEDS TO BE DONE: write a function which simply reads n number of bits from the image and returns them as a string
 	//Create a string of 0's and 1's that has the length of the message
 	messageLength := ""
@@ -51,42 +78,69 @@ func decodeImage(img *image.RGBA) {
 
 	//Get the values at the first 12 channels (4 pixels)
 	for x < 4 {
-		r, g, b, _ := img.At(x, y).RGBA()
-		r = r >> 8
-		g = g >> 8
-		b = b >> 8
-		messageLength += string(r%2) + string(g%2) + string(b%2)
+		messageLength += readPixel(img.At(x, y))
 		x++
 	}
 
 	//Parse the message length string to an integer
-	length, _ := strconv.ParseInt(messageLength, 2, 32)
+	l, _ := strconv.ParseInt(messageLength, 2, 32)
+	length := int(l)
+
+	fmt.Println("message length:", length)
 
 	bitMessage := ""
 	bitCounter := 0
 
-	for bitCounter < int(length) {
-		r, g, b, _ := img.At(x, y).RGBA()
-		r = r >> 8
-		g = g >> 8
-		b = b >> 8
-		cols := []uint32{r, g, b}
-		//we need to go through each channel: check if it corresponds with current bit
-		//0 = even, 1 = odd
-		for _, c := range cols {
-			bitMessage += string(c % 2)
-			bitCounter++
-			if bitCounter >= len(messageLength) {
-				break
-			}
+	for bitCounter < length {
+		messagePart := readPixel(img.At(x, y))
+		if (length - bitCounter) <= 3 { //if we have less than 3 bits left to read, don't add entire message
+			messagePart = messagePart[0 : length-bitCounter]
 		}
+		bitMessage += messagePart
+		bitCounter += 3
 		x++
 		if x >= bounds.Max.X {
 			x = 0
 			y++
 		}
 	}
+	//We now have a list of bits in the form of a string
+	//looking a bit like this: '101100110101'
+	//we need to split it in 7s, where each group of seven represents an int, in turn
+	//representing an ASCII character
+	if len(bitMessage)%7 != 0 {
+		fmt.Println("error, bitMessage does not contain a multiple of 7 bits")
+	}
 
+	ints := make([]int, len(bitMessage)/7)
+	chars := make([]rune, len(bitMessage)/7)
+	for i := 0; i < len(bitMessage); i += 7 {
+		part := bitMessage[i : i+7]
+		asciiInt, _ := strconv.ParseInt(part, 2, 32)
+		ints[i/7] = int(asciiInt)
+		chars[i/7] = rune(asciiInt)
+	}
+	fmt.Println(string(chars))
+
+}
+
+func encodeMessage(message string, rgba *image.RGBA) {
+	//Get the ASCII value for each character in the message
+	ints := getInts(message)
+
+	//This is a list of string, where each string is a binary representation of each character in the message
+	//Convert each ASCII value into binary
+	//Concatenate everything into one whole string
+	strBs := getBytes(ints)
+
+	//Process img with bytes
+	processImage(rgba, strBs)
+
+	//
+	f, _ := os.Create("out.png")
+	defer f.Close()
+	e := png.Encode(f, rgba)
+	fmt.Println("Success", e)
 }
 
 //Takes image and message to write into image
@@ -96,9 +150,46 @@ func processImage(img *image.RGBA, strBs string) {
 	//i = current index in the message
 	x, y, i := 0, 0, 0
 
+	//bitLength is simply the number of bits used to describe the message
+	bitLength := getBytes([]int{len(strBs)})
+
+	//make sure it is 12 bits long
+	bitLength = padBitString(bitLength, 12)
+
+	for j := 0; j < 4; j++ {
+		r, g, b, a := img.At(x, y).RGBA()
+
+		//Make the pixel values between 0 and 255
+		r = r >> 8
+		g = g >> 8
+		b = b >> 8
+		cols := []uint32{r, g, b}
+		//we need to go through each channel: check if it corresponds with current bit
+		//0 = even, 1 = odd
+		for k, c := range cols {
+			//Convert character to string, then to an integer (0 or 1)
+			curVal, _ := strconv.Atoi(string(bitLength[3*j+k]))
+			//Check parity of channel against parity of current bit in message
+			if int(c%2) != curVal {
+				toggleOddEven(&c)
+				cols[k] = c
+			}
+		}
+		img.Set(x, y, color.RGBA{uint8(cols[0]), uint8(cols[1]), uint8(cols[2]), uint8(a)})
+		x++
+		if x == bounds.Max.X {
+			x = 0
+			y++
+		}
+
+	}
+
+	//write bitLength (number of bits composing message) to signify decoder how much to read
+	fmt.Println("bitLength", bitLength)
+
 	for i < len(strBs) {
 		//Get the RGB values at (x,y)
-		r, g, b, _ := img.At(x, y).RGBA()
+		r, g, b, a := img.At(x, y).RGBA()
 		//Make the pixel values between 0 and 255
 		r = r >> 8
 		g = g >> 8
@@ -109,7 +200,6 @@ func processImage(img *image.RGBA, strBs string) {
 		for j, c := range cols {
 			//Convert character to string, then to an integer (0 or 1)
 			curVal, _ := strconv.Atoi(string(strBs[i]))
-			fmt.Println("curVal", curVal)
 			//Check parity of channel against parity of current bit in message
 			if int(c%2) != curVal {
 				toggleOddEven(&c)
@@ -121,7 +211,7 @@ func processImage(img *image.RGBA, strBs string) {
 				break
 			}
 		}
-		img.Set(x, y, color.RGBA{uint8(cols[0]), uint8(cols[1]), uint8(cols[2]), 0})
+		img.Set(x, y, color.RGBA{uint8(cols[0]), uint8(cols[1]), uint8(cols[2]), uint8(a)})
 		x++
 		if x == bounds.Max.X {
 			x = 0
@@ -131,7 +221,6 @@ func processImage(img *image.RGBA, strBs string) {
 }
 
 func main() {
-	fmt.Println("hey")
 	fileName := os.Args[1]
 	message := ""
 	encode := false
@@ -149,31 +238,21 @@ func main() {
 	}
 
 	//Read the image
-	img, err := jpeg.Decode(reader)
+	img, err := png.Decode(reader)
 
 	if err != nil {
 		fmt.Println("failed to read file", err)
 		os.Exit(1)
 	}
 
-	//Here, we do 2 different things, based on the value of encode: call Frankie's code if false, and Jasmine's code if true
-
-	//Get the ASCII value for each character in the message
-	ints := getInts(message)
-
-	//This is a list of string, where each string is a binary representation of each character in the message
-	//Convert each ASCII value into binary
-	//Concatenate everything into one whole string
-	strBs := getBytes(ints)
-
 	//Convert image.Image (read-only) to image.RGBA (editable format)
 	rgba := convertImage(img)
 
-	//Process img with bytes
-	processImage(rgba, strBs)
+	//Here, we do 2 different things, based on the value of encode: call Frankie's code if false, and Jasmine's code if true
+	if encode {
+		encodeMessage(message, rgba)
+	} else {
+		decodeMessage(rgba)
+	}
 
-	//
-	f, _ := os.Create("out.jpg")
-	e := jpeg.Encode(f, rgba, nil)
-	fmt.Println("Success", e)
 }
